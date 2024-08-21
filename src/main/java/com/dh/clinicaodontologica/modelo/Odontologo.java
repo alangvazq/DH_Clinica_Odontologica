6ccc53566d6047ea8077ae239cab4d7e2273d00e
alangvazq <alan.g.vazq@gmail.com>
2024-08-20 19:49:32 -0600

    Modelo de odontologo

---
+++ src/main/java/com/dh/clinicaodontologica/modelo/Odontologo.java
@@ -0,0 +1,56 @@
+package com.dh.clinicaodontologica.modelo;
+
+public class Odontologo {
+    private Long id;
+    private String nombre;
+    private String apellido;
+    private String matricula;
+
+    public Odontologo(String nombre, String apellido, String matricula) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.matricula = matricula;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    @Override
+    public String toString() {
+        return "Odontologo{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                ", apellido='" + apellido + '\'' +
+                ", matricula='" + matricula + '\'' +
+                '}';
+    }
+}
