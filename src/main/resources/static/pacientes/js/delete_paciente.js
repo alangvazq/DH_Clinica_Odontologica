9e4fc711afadbb2157078fe613124fb703076dec
Ayxa Chaverra <achaverrar@gmail.com>
2024-09-09 21:06:29 -0500

    handle case of no content response to delete requests in the frontend side of the application

--- src/main/resources/static/pacientes/js/delete_paciente.js
+++ src/main/resources/static/pacientes/js/delete_paciente.js
@@ -3,7 +3,14 @@ function deleteBy(id) {
   const settings = {
     method: "DELETE",
   };
-  fetch(url, settings).then((response) => response.json());
+
+  fetch(url, settings).then((response) => {
+    if (response.status >= 400) {
+      return response.json().then(({ mensaje }) => {
+        throw new Error(mensaje || "Error, intente nuevamente");
+      });
+    }
+  });
 
   let row_id = "#tr_" + id;
   document.querySelector(row_id).remove();
