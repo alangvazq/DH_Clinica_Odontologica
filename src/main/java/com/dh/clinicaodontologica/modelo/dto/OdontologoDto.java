4709d83e73915196954515db59db8329ebea2d00
alangvazq <alan.g.vazq@gmail.com>
2024-09-11 12:31:18 -0600

    Odontologo DTO

---
+++ src/main/java/com/dh/clinicaodontologica/modelo/dto/OdontologoDto.java
@@ -0,0 +1,23 @@
+package com.dh.clinicaodontologica.modelo.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Builder
+@Getter
+@Setter
+public class OdontologoDto {
+    private Long id;
+
+    @NotBlank(message = "El nombre no puede quedar en blanco")
+    private String nombre;
+
+    @NotBlank(message = "El apellido no puede quedar en blanco")
+    private String apellido;
+
+    @NotBlank(message = "La matricula no puede quedar en blanco")
+    private String matricula;
+
+}
