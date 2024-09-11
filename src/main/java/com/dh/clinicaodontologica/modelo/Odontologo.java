cd55735d0c173cf967fec9145a3bf3654eddf9b4
alangvazq <alan.g.vazq@gmail.com>
2024-09-11 12:18:29 -0600

    Modelo odontologo actuaÃlizado

--- src/main/java/com/dh/clinicaodontologica/modelo/Odontologo.java
+++ src/main/java/com/dh/clinicaodontologica/modelo/Odontologo.java
@@ -2,100 +2,33 @@ package com.dh.clinicaodontologica.modelo;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
 import java.util.Objects;
 import java.util.Set;
 
 @Entity
 @Table(name = "odontologos")
+@Getter
+@Setter
+@ToString
 public class Odontologo {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false)
     private String nombre;
+
+    @Column(nullable = false)
     private String apellido;
+
+    @Column(nullable = false, unique = true)
     private String matricula;
 
     @OneToMany(mappedBy = "odontologo")
     @JsonIgnore
     private Set<Turno> turnos;
-
-    public Odontologo(Long id, String nombre, String apellido, String matricula) {
-        this.id = id;
-        this.nombre = nombre;
-        this.apellido = apellido;
-        this.matricula = matricula;
-    }
-
-    public Odontologo(String nombre, String apellido, String matricula) {
-        this.nombre = nombre;
-        this.apellido = apellido;
-        this.matricula = matricula;
-    }
-
-    public Odontologo() {
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getApellido() {
-        return apellido;
-    }
-
-    public void setApellido(String apellido) {
-        this.apellido = apellido;
-    }
-
-    public String getMatricula() {
-        return matricula;
-    }
-
-    public void setMatricula(String matricula) {
-        this.matricula = matricula;
-    }
-
-    public Set<Turno> getTurnos() {
-        return turnos;
-    }
-
-    public void setTurnos(Set<Turno> turnos) {
-        this.turnos = turnos;
-    }
-
-    @Override
-    public String toString() {
-        return "Odontologo{" +
-                "id=" + id +
-                ", nombre='" + nombre + '\'' +
-                ", apellido='" + apellido + '\'' +
-                ", matricula='" + matricula + '\'' +
-                '}';
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Odontologo that = (Odontologo) o;
-        return Objects.equals(id, that.id) && Objects.equals(nombre, that.nombre) && Objects.equals(apellido, that.apellido) && Objects.equals(matricula, that.matricula);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, nombre, apellido, matricula);
-    }
 }
